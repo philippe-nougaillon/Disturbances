74ccaa05c421f051040b7d8b361064723b7b10e9
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2022-02-26 18:34:14 +0100

    local time FR

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,62 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 import LocalTime from 'local-time'
+
+LocalTime.config.i18n["fr"] = {
+    date : {
+        dayNames: [
+            "Dimanche",
+            "Lundi",
+            "Mardi",
+            "Mercredi",
+            "Jeudi",
+            "Vendredi",
+            "Samedi",
+        ],          
+        abbrMonthNames: [
+            "Jan",
+            "Fév",
+            "Mar",
+            "Avr",
+            "Mai",
+            "Juin",
+            "Juillet",
+            "Aoüt",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Déc",
+        ],
+        yesterday: "hier",
+        today: "aujourd'hui",
+        tomorrow: "demain",
+        on: "le {date}",
+        formats: {
+            default: "%e %b %Y",
+            thisYear: "%e %b",
+        },
+    },    
+    time: {
+        elapsed: "il y a {time}",
+        second: "seconde",
+        seconds: "secondes",
+        minute: "minute",
+        minutes: "minutes",
+        hour: "heure",
+        hours: "heures",   
+        singular: "une {time}", 
+        formats: {
+            default: "%l:%M",
+        },
+    },
+    datetime: {
+        at: "{date} à {time}",
+        formats: {
+            default: "%B %e, %Y à %l:%m",
+        }
+    }
+}
+   
+LocalTime.config.locale = "fr"
 LocalTime.start()
 
