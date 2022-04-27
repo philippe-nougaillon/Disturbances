88a9a6142331f86229cbbc6bda241124b328eeff
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-04-27 16:48:05 +0200

    status page sql request done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:status]
 
   def status
-    @disturbances = Disturbance.select(:origine,:perturbation, :information, :created_at).where("DATE(disturbances.created_at) = ?", Date.today).group(:origine, :perturbation, :information, :created_at)
+    @sources = Source.order(:gare)
   end
 end
