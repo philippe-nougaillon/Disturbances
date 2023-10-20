dc6bb1fde5ad5f7ed495d355fb2b8e55995f0aae
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-10-20 09:35:15 +0200

    scraper : retry config added

--- app/services/disturbances_get.rb
+++ app/services/disturbances_get.rb
@@ -28,7 +28,8 @@ class GithubSpider < Tanakai::Base
   @start_urls = Source.all.pluck(:url)
   @config = {
     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
-    before_request: { delay: 4..7 }
+    before_request: { delay: 4..7 },
+    retry_request_errors: [{ skip_on_failure: true }]
   }
   
   def parse(response, url:, data: {})
